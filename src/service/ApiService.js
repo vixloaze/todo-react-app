f6e6d9f6861e85cc62ff3e92bf8a683b2349e2b1
조성현 <agllwy116@naver.com>
2022-12-29 16:58:01 +0900

    5.3 로컬 스토리지를 이용한 액세스 토큰 관리(액세스 토큰 저장)

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,17 @@
 import { API_BASE_URL } from "../config/api-config";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    // 로컬 스토리지에서 ACCESS TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken !== null){
+        headers.append("Authorization", "Bearer "+accessToken);
+    }
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -19,8 +26,7 @@ export function call(api, method, request) {
         } else if (response.status === 403) {
             window.location.href = "/login"; // redirect
         } else {
-            Promise.reject(response);
-            throw Error(response);
+            new Error(response);
         }
     }).catch((error) => {
         console.log("http error");
@@ -32,6 +38,8 @@ export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO)
         .then((response) => {
             if(response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
                 // token이 존재하는 경우 Todo 화면으로 리디렉트
                 window.location.href = "/";
             }
