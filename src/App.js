3609030404be1f7cb9bf7492c2554eec1cddd9b2
조성현 <agllwy116@naver.com>
2022-12-31 16:03:20 +0900

    네비게이션 바 추가 및 UI 글리치 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Container, List, Paper } from '@mui/material';
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography } from '@mui/material';
 import React, { useState, useEffect } from 'react';
-import {call} from "../src/service/ApiService";
+import {call, signout} from "../src/service/ApiService";
 import './App.css';
 import Todo from "./components/Todo";
 import AddTodo from './components/AddTodo';
@@ -8,10 +8,12 @@ import AddTodo from './components/AddTodo';
 function App() {
 
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     call("/todo","GET", null)
     .then((response) => setItems(response.data));
+    setLoading(false);
   },[]);
 
   const addItem = (item) => {
@@ -40,15 +42,46 @@ function App() {
         </Paper>
       );
 
-      
+    // navigationBar 추가
+    let navigationBar = (
+      <AppBar position='static'>
+        <Toolbar>
+          <Grid justifyContent="space-between" container>
+            <Grid item>
+              <Typography variant='h6'>오늘의 할 일</Typography>
+            </Grid>
+            <Grid item>
+              <Button color='inherit' raised onClick={signout}>
+                로그아웃
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    )
+
+    // 로딩중이 아닐 때 렌더링할 부분
+    let todoListPage = (
+      <div className="App">
+        {navigationBar}
+        <Container maxWidth="md">
+          <AddTodo addItem={addItem}/>
+          <div className='TodoList'>{todoItems}</div>
+        </Container>
+    </div>
+    );
+
+    // 로딩 중일 때 렌더링할 부분
+    let loadingPage = <h1>로딩 중....</h1>;
+    let content = loadingPage;
+
+    if (!loading) {
+      // 로딩 중이 아니면 todoListPage를 선택
+      content = todoListPage;
+    }
 
   return (
-    <div className="App">
-      <Container maxWidth="md">
-        <AddTodo addItem={addItem}/>
-        <div className='TodoList'>{todoItems}</div>
-      </Container>
-    </div>
+    <div className="App">{content}</div>
   );
 }
 export default App;
